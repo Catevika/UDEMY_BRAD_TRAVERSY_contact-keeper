2b746f4592dd2a99360484f38b18de1c569525a6
Catevika <dominique.bello@outlook.fr>
2022-02-05 13:56:40 +0100

    Update

--- config/db.js
+++ config/db.js
@@ -1,18 +1,32 @@
 const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
+const colors = require('colors');
+
+// const connectDB = async () => {
+// 	try {
+// 		await mongoose.connect(db, {
+// 			useNewUrlParser: true,
+// 			useCreateIndex: true,
+// 			userFindAndModify: false,
+// 			useUnifiedTopology: true
+// 		});
+// 		console.log('MongoDB Connected ...');
+// 	} catch (err) {
+// 		console.error(err.message);
+// 		process.exit(1);
+// 	}
+// };
+
+// module.exports = connectDB;
 
 const connectDB = async () => {
 	try {
-		await mongoose.connect(db, {
-			useNewUrlParser: true,
-			useCreateIndex: true,
-			userFindAndModify: false,
-			useUnifiedTopology: true
-		});
-		console.log('MongoDB Connected ...');
-	} catch (err) {
-		console.error(err.message);
+		const conn = await mongoose.connect(db);
+
+		console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline);
+	} catch (error) {
+		console.log(`Error: ${error.message}`.red.underline.bold);
 		process.exit(1);
 	}
 };
